de6ce91ad88ffe3f375a768c3a0b42f67f9a30b1
Jens Restemeier <jens.restemeier@gmail.com>
2022-05-19 21:56:16 +0100

    removed some unused template code from the c file

--- pixel_art_scalers.c
+++ pixel_art_scalers.c
@@ -1,6 +1,4 @@
 #include <Python.h>
-#include "structmember.h"
-#include <stdbool.h>
 
 #include "hqx.h"
 #include "scaler_nearestneighbor.h"
@@ -8,113 +6,6 @@
 #include "xbr_filters.h"
 
 #define PY3K (PY_VERSION_HEX >= 0x03000000)
-
-#if PY3K
-#define Py_TPFLAGS_CHECKTYPES 0
-#endif
-
-#if !PY3K
-#define Py_RETURN_NOTIMPLEMENTED return Py_INCREF(Py_NotImplemented), Py_NotImplemented
-#define PyLong_AS_LONG(op) PyLong_AsLong(op)
-#define PyExtNumber_Check(op) (PyLong_Check(op) || PyInt_Check(op) || PyFloat_Check(op) || PyBool_Check(op))
-#endif
-bool PyExtNumber_Check(PyObject* arg) {
-#if PY3K
-	if (PyFloat_Check(arg) || PyLong_Check(arg) || PyBool_Check(arg)) {
-#else
-	if (PyFloat_Check(arg) || PyInt_Check(arg) || PyLong_Check(arg) ||  PyBool_Check(arg)) {
-#endif
-		return true;
-	}
-	if (arg->ob_type->tp_as_number != NULL && arg->ob_type->tp_as_number->nb_float != NULL) {
-		PyObject* temp = PyNumber_Float(arg);
-		if (temp != NULL) {
-			Py_DECREF(temp);
-			return true;
-		}
-		PyErr_Clear();
-	}
-	return false;
-}
-
-double PyExtNumber_AsDouble(PyObject* arg) {
-	if (PyFloat_Check(arg)) {
-		return PyFloat_AS_DOUBLE(arg);
-	}
-#if !PY3K
-	if (PyInt_Check(arg)) {
-		return (double)PyInt_AS_LONG(arg);
-	}
-#endif
-	if (PyLong_Check(arg)) {
-		return (double)PyLong_AsDouble(arg);
-	}
-	if (PyBool_Check(arg)) {
-		return (arg == Py_True) ? 1.0 : 0.0;
-	}
-	PyObject* arg_as_float = PyNumber_Float(arg);
-	double out = PyFloat_AS_DOUBLE(arg_as_float);
-	Py_DECREF(arg_as_float);
-	return out;
-}
-
-long PyExtNumber_AsLong(PyObject* arg) {
-#if !PY3K
-	if (PyInt_Check(arg)) {
-		return PyInt_AS_LONG(arg);
-	}
-#endif
-	if (PyLong_Check(arg)) {
-		return PyLong_AS_LONG(arg);
-	}
-	if (PyFloat_Check(arg)) {
-		return (long)PyFloat_AS_DOUBLE(arg);
-	}
-	if (PyBool_Check(arg)) {
-		return (arg == Py_True) ? 1 : 0;
-	}
-	PyObject* arg_as_long = PyNumber_Long(arg);
-	long out = PyLong_AS_LONG(arg_as_long);
-	Py_DECREF(arg_as_long);
-	return out;
-}
-
-float PyExtNumber_AsFloat(PyObject* arg) {
-	if (PyFloat_Check(arg)) {
-		return (float)PyFloat_AS_DOUBLE(arg);
-	}
-#if !PY3K
-	if (PyInt_Check(arg)) {
-		return (float)PyInt_AS_LONG(arg);
-	}
-#endif
-	if (PyLong_Check(arg)) {
-		return (float)PyLong_AS_LONG(arg);
-	}
-	if (PyBool_Check(arg)) {
-		return (arg == Py_True) ? 1.f : 0.f;
-	}
-	PyObject* arg_as_float = PyNumber_Float(arg);
-	float out = (float)PyFloat_AS_DOUBLE(arg_as_float);
-	Py_DECREF(arg_as_float);
-	return out;
-}
-
-
-#define Py_IS_NOTIMPLEMENTED(op) (op == NULL || (PyObject*)op == Py_NotImplemented) // find out if op is NULL or NotImplemented
-
-#define PyType_AS_CSTRING(op) op->ob_type->tp_name
-
-#define Py_RAISE_TYPEERROR_O(str, obj) PyErr_Format(PyExc_TypeError, "%s'%s'", str, PyType_AS_CSTRING(obj))
-#define Py_RAISE_TYPEERROR_2O(str, obj1, obj2) PyErr_Format(PyExc_TypeError, "%s'%s' and '%s'", str, PyType_AS_CSTRING(obj1), PyType_AS_CSTRING(obj2))
-
-static char * attr_name_to_cstr(PyObject * name) {
-#if PY3K
-	return PyBytes_AsString(PyUnicode_AsASCIIString(name));
-#else
-	return PyString_AsString(name);
-#endif
-}
 
 static PyObject*
 py_hq2x_32(PyObject* self, PyObject* args) {
@@ -351,10 +242,6 @@ py_scaler_scalex_4x(PyObject* self, PyObject* args) {
 }
 
 static PyMethodDef templatemethods[] = {
-	/* PyMethodDef, a structure used to describe a method of an extension type.
-		* reference:
-		* https://docs.python.org/3/c-api/structures.html#c.PyMethodDef
-		*/
 	{ "py_hq2x_32", (PyCFunction)py_hq2x_32, METH_VARARGS, "HQ2X algorithm" },
 	{ "py_hq3x_32", (PyCFunction)py_hq3x_32, METH_VARARGS, "HQ2X algorithm" },
 	{ "py_hq4x_32", (PyCFunction)py_hq4x_32, METH_VARARGS, "HQ2X algorithm" },
@@ -372,7 +259,7 @@ static PyModuleDef templatemodule = {
 
 	PyModuleDef_HEAD_INIT,
 	"_pixel_art_scalers",
-	"A simple template for Python's C-API",
+	"A simple wrapper for gimp-pixel-art-scaler",
 	-1,
 	templatemethods, NULL, NULL, NULL, NULL
 };
@@ -393,7 +280,7 @@ PyMODINIT_FUNC
 #if PY3K
 	m = PyModule_Create(&templatemodule);
 #else
-	m = Py_InitModule3("_pixel_art_scalers", templatemethods, "A simple template for Python's C-API");
+	m = Py_InitModule3("_pixel_art_scalers", templatemethods, "A simple wrapper for gimp-pixel-art-scaler");
 #endif
 	if (m == NULL)
 #if PY3K
